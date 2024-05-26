e91a6c374228a7c39a1ad1e4a39afd9407f389f5
Shawn <shawn@qwil.io>
2024-05-26 17:30:16 +0100

    Apex component for retrieving SDK token

--- force-app/main/default/lwc/qwilEmbed/qwilEmbed.js
+++ force-app/main/default/lwc/qwilEmbed/qwilEmbed.js
@@ -1,27 +1,61 @@
 import { LightningElement } from 'lwc';
 import { loadScript } from "lightning/platformResourceLoader";
 import QwilApiLib from "@salesforce/resourceUrl/QwilApiLib";
+import authenticate from '@salesforce/apex/QwilSdkAuth.authenticate';
 
 export default class QwilEmbed extends LightningElement {
     api;
+    error;
+    credentials;
+    loaded = false;
 
-    async renderedCallback() {
-        await loadScript(this, QwilApiLib);  // this exposes api obj on window.QwilApi
+
+    async connectedCallback() {
+        await Promise.all([
+            loadScript(this, QwilApiLib),  // this exposes api obj on window.QwilApi
+            this.retrieveCredentials(), // this populates either this.credentials or this.error
+        ]);
+        
+
+        if (this.error) {
+            return;
+        } 
+
+        const { token, endpoint } = this.credentials;
+
         const container = this.template.querySelector('div.qwil-container');
-
         this.api = new window.QwilApi({
-            token: 'token', // TODO: load this from /sys-api query
-            endpoint: 'https://endpoint',  // TODO: load this from /sys-api query
+            token,
+            endpoint,
             targetElement: container,
             options: {
-                customUrl: 'https://shawnchin.github.io/hosting/qwil-api-debug', // TODO: point to https://sdk.qwil.io
+                customUrl: 'https://sdk-beta.qwil.network/',
             },
             onLoad: () => {
                 console.log('Qwil login successful'); // TODO: register auth-expired handler
+                this.loaded = true;
             },
             onError: () => {
                 console.error('Qwil login failed'); // TODO: error handling
+                this.loaded = true;
             },
         });
     }
+
+    async retrieveCredentials() {
+        try {
+            this.credentials = await authenticate();
+        } catch (error) {
+            console.error(error);
+            this.error = error;
+        }
+    }
+
+    async disconnectedCallback() {
+        if (this.api) {
+            this.api.dispose();
+            this.api = null;
+        }
+    }
+
 }
